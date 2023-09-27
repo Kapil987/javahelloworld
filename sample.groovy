43ee65aee9bf9500d8c395d939eea5c335980ec3
KAPIL <kapil0123@gmail.com>
2023-09-27 22:03:43 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -6,46 +6,16 @@ def agentName = 'algoworks-dev-server' // mock value for testing
 
 pipelineJob("${projectName}-Pipeline") {
     description("Pipeline job for ${projectName}")
+    parameters {
+        stringParam('AGENT_NAME', agentName, 'The name of the agent to run the build on')
+    }
 
     definition {
-        cps {
-          
-            script("""
-                pipeline {
-                    agent {
-                        label '${agentName}'
-                    }
-
-                    stages {
-                        stage('Build') {
-                            steps {
-                                withCredentials([aws(credentialsId: 'pm2-stage-aws-creds', accessKeyVariable: 'AWS_ACCESS_KEY', secretKeyVariable: 'AWS_SECRET_KEY')]) {
-                                 sh '''#!/bin/bash
-                                 export AWS_ACCESS_KEY_ID=${env.AWS_ACCESS_KEY}
-                                 export AWS_SECRET_ACCESS_KEY=${env.AWS_SECRET_KEY}
-                                 aws s3 ls
-                                '''
-                                 }
-                               
-                                echo 'This is a build stages'
-                                sleep 2
-                            }
-                        }
-                        stage('Test') {
-                            steps {
-                                echo 'This is a test stage'
-                                sleep 3
-                            }
-                        }
-                        stage('Deploy') {
-                            steps {
-                                echo 'This is a deploy stage'
-                                sleep 4
-                            }
-                        }
-                    }
-                }
-            """)
+        cpsScm {
+            scm {
+                git("https://github.com/Kapil987/javahelloworld.git", 'main')  // Adjust as necessary
+            }
+        scriptPath('Jenkinsfile') 
             sandbox(false)
         }
     }
