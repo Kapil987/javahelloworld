b6f93c8b0cb7031a06672ff61b4f0eb579eafd30
KAPIL <kapil0123@gmail.com>
2023-09-27 22:15:42 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -2,14 +2,16 @@
 //def agentName = params['AGENT_NAME'].trim()
 def projectName = 'TestProject2' // mock value for testing
 def agentName = 'algoworks-dev-server' // mock value for testing
+def gitRepoURL = 'YOUR_GIT_REPO_URL'
+def gitCredentialsId = 'YOUR_CREDENTIALS_ID'  // Replace with your actual Git credentials ID
 
 
 pipelineJob("${projectName}-Pipeline") {
     description("Pipeline job for ${projectName}")
-    parameters {
-        stringParam('AGENT_NAME', agentName, 'The name of the agent to run the build on')
-    }
-
+    //parameters {
+    //    stringParam('AGENT_NAME', agentName, 'The name of the agent to run the build on')
+    //}
+    
     definition {
         cpsScm {
             scm {
