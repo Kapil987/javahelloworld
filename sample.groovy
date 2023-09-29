6482174f4921fbc04a3fdad24ea527f06f1670f8
KAPIL <kapil0123@gmail.com>
2023-09-29 17:59:32 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -6,10 +6,10 @@ def projects = [
 ]
 
 // Lists for repo URLs, credentials IDs, agent names, and branch names
-def repoURLs = ['None', '']
-def credentialsIds = ['Cred1', 'Cred2']
+//def repoURLs = ['None', '']
+//def credentialsIds = ['Cred1', 'Cred2']
 def agentNames = ['algoworks-dev-server', 'algoworks-dev-server',]
-def branchNames = ['main', 'develop']
+//def branchNames = ['main', 'develop']
 
 // Variable to determine the number of days to retain builds
 def daysToKeep = 7
@@ -19,11 +19,6 @@ for (int i = 0; i < projects.size(); i++) {
     def project = projects[i]
     def projectName = "${project.name}-${project.environment}-${project.team}-${project.job}"
     
-    def currentRepoURL = repoURLs[i]
-    def currentCredentialsId = credentialsIds[i]
-    def currentAgentName = agentNames[i]
-    def currentBranchName = branchNames[i]
-
     pipelineJob("${projectName}-Pipeline") {
         description("Pipeline job for ${projectName}")
         
@@ -32,27 +27,11 @@ for (int i = 0; i < projects.size(); i++) {
             numToKeep(daysToKeep)
         }
 
-        // Use agent, repo URL, credentials, and branch name as needed.
-        if (currentRepoURL && currentRepoURL != 'None' && currentRepoURL != '') { 
-            definition {
-                cpsScm {
-                    scm {
-                        git {
-                            remote {
-                                url(currentRepoURL)
-                                credentials(currentCredentialsId)
-                            }
-                            branch(currentBranchName)
-                        }
-                    }
-                    scriptPath('Jenkinsfile-s3-cloudfront')
-                }
+        // Set the script path
+        definition {
+            cps {
+                scriptPath('Jenkinsfile-s3-cloudfront')
             }
-        } else {
-                cps {
-                    // No scm block here, as there's no SCM for this job
-                   script(readFileFromWorkspace('Jenkinsfile-s3-cloudfront'))
-                }
-            }
+        }
     }
 }
