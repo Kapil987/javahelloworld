88e0bc023074544675f3f4fb72dc34ad5f2717ff
KAPIL <kapil0123@gmail.com>
2023-10-01 13:53:14 +0530

    Update freestyle.groovy

--- freestyle.groovy
+++ freestyle.groovy
@@ -1,3 +1,5 @@
+// Variables that may need modification
+
 def AGENT_LABEL = "algoworks-dev-server"
 
 // List of project details
@@ -15,10 +17,39 @@ def daysToKeep = 7
 // Shell script to be executed in the freestyle job
 def shellScript = '''
 #!/bin/bash
-set -x
+set -e
+
+# Shell variables to be set
+CLOUDFRONT_DIST_ID=""
+BUCKET_NAME=""
+
+# Check if the variables are set and not empty
+if [ -z "$CLOUDFRONT_DIST_ID" ] || [ -z "$BUCKET_NAME" ]; then
+    echo "Error: Either CLOUDFRONT_DIST_ID or BUCKET_NAME is not set."
+    exit 1
+fi
+
+echo -e "\n########### exporting creds ###########"
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 aws s3 ls
+
+echo -e "\n########### unzipping file ###########"
+mv dist ${params.dist}
+unzip ${params.dist}
+sleep 5
+
+echo -e "\n########### uploading it to s3 ###########"
+if [ -d "${WORKSPACE}/dist" ]
+then
+    cd ${WORKSPACE}/dist/
+    aws s3 cp . s3://${BUCKET_NAME}
+    aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DIST_ID} --paths "/*"
+    rm -rf ${WORKSPACE}/*
+else
+    exit 1
+fi
+
 '''
 
 // Loop over projects
